4cd6d1373adbd521175dc92356b60be69215fecc
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 18:44:01 -0300

    Add database relationship in Cliente class

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +27,7 @@ public class Cliente implements Serializable {
 	private BigDecimal rendaMensal;
 	
 	// Relationship Variables
+	@OneToMany(mappedBy="cliente")
 	private List<Emprestimo>emprestimos;
 	
 	Cliente()
