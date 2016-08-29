3257a79d9b116c78d54649d044be3dfe43849e21
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:44:30 -0300

    Add Getting and Setters in Livro class
    Adding getting and setters methods in livro class

--- src/main/java/dominio/Livro.java
+++ src/main/java/dominio/Livro.java
@@ -34,6 +34,120 @@ public class Livro {
 		this.categoria = categoria;
 		this.itens = new ArrayList<>();
 	}
+
+	/**
+	 * @return the codLivro
+	 */
+	public Integer getCodLivro() {
+		return codLivro;
+	}
+
+	/**
+	 * @param codLivro the codLivro to set
+	 */
+	public void setCodLivro(Integer codLivro) {
+		this.codLivro = codLivro;
+	}
+
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	/**
+	 * @return the isbn
+	 */
+	public String getIsbn() {
+		return isbn;
+	}
+
+	/**
+	 * @param isbn the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	/**
+	 * @return the paginas
+	 */
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	/**
+	 * @param paginas the paginas to set
+	 */
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+
+	/**
+	 * @return the valorDiario
+	 */
+	public BigDecimal getValorDiario() {
+		return valorDiario;
+	}
+
+	/**
+	 * @param valorDiario the valorDiario to set
+	 */
+	public void setValorDiario(BigDecimal valorDiario) {
+		this.valorDiario = valorDiario;
+	}
+
+	/**
+	 * @return the editora
+	 */
+	public Editora getEditora() {
+		return editora;
+	}
+
+	/**
+	 * @param editora the editora to set
+	 */
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	/**
+	 * @return the itens
+	 */
+	public List<ItemEmprestimo> getItens() {
+		return itens;
+	}
+
+	/**
+	 * @param itens the itens to set
+	 */
+	public void setItens(List<ItemEmprestimo> itens) {
+		this.itens = itens;
+	}
+	
+	
 	
 	
 
