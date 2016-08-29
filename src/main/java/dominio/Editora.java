11c1f862d63e4646c6bac09fd180fb8cc5aa899a
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 18:30:34 -0300

    Add @Entity, @Table with name and creating primary_key in Editora class

--- src/main/java/dominio/Editora.java
+++ src/main/java/dominio/Editora.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_editoras")
 public class Editora implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	// Internal Variables
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer codEditora;
 	private String nome;
 	
