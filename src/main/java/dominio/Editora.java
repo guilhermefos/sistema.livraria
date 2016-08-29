cd52c6486db1f39c60568505a8027e024d4ea636
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:22:29 -0300

    Add relationship in class Editora
    adding relationship variables in class Editora

--- src/main/java/dominio/Editora.java
+++ src/main/java/dominio/Editora.java
@@ -1,9 +1,14 @@
 package dominio;
 
+import java.util.List;
+
 public class Editora {
 	
 	// Internal Variables
 	private Integer codEditora;
 	private String nome;
+	
+	// Relationship Variables
+	private List<Livro>livros;
 
 }
