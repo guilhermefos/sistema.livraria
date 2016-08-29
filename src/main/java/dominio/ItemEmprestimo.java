e7cdbc0fdeb17ae40d30a08793d2914dd07d0cd8
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:48:30 -0300

    Add Getting and Setters in ItemEmprestimo class
    Adding getting and setters methods in ItemEmprestimo class

--- src/main/java/dominio/ItemEmprestimo.java
+++ src/main/java/dominio/ItemEmprestimo.java
@@ -20,4 +20,62 @@ public class ItemEmprestimo {
 		this.emprestimo = emprestimo;
 	}
 
+	/**
+	 * @return the codItemEmprestimo
+	 */
+	public Integer getCodItemEmprestimo() {
+		return codItemEmprestimo;
+	}
+
+	/**
+	 * @param codItemEmprestimo the codItemEmprestimo to set
+	 */
+	public void setCodItemEmprestimo(Integer codItemEmprestimo) {
+		this.codItemEmprestimo = codItemEmprestimo;
+	}
+
+	/**
+	 * @return the quantidade
+	 */
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	/**
+	 * @param quantidade the quantidade to set
+	 */
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	/**
+	 * @return the livro
+	 */
+	public Livro getLivro() {
+		return livro;
+	}
+
+	/**
+	 * @param livro the livro to set
+	 */
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	/**
+	 * @return the emprestimo
+	 */
+	public Emprestimo getEmprestimo() {
+		return emprestimo;
+	}
+
+	/**
+	 * @param emprestimo the emprestimo to set
+	 */
+	public void setEmprestimo(Emprestimo emprestimo) {
+		this.emprestimo = emprestimo;
+	}
+	
+	
+
 }
