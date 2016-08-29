8fc4c1db8438adecd52cc0a5b0897af202a0e04f
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:29:27 -0300

    Add relationship in class ItemEmprestimo
    adding relationship variables in class ItemEmprestimo

--- src/main/java/dominio/ItemEmprestimo.java
+++ src/main/java/dominio/ItemEmprestimo.java
@@ -5,5 +5,9 @@ public class ItemEmprestimo {
 	// Internal Variables
 	private Integer codItemEmprestimo;
 	private Integer quantidade;
+	
+	// Relationship Variables
+	private Livro livro;
+	private Emprestimo emprestimo;
 
 }
