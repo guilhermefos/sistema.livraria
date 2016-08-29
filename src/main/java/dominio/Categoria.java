83618394a13a00b83e89cf77cb24ca0f5b491dda
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:24:03 -0300

    Add relationship in class Categoria
    adding relationship variables in class Categoria

--- src/main/java/dominio/Categoria.java
+++ src/main/java/dominio/Categoria.java
@@ -1,9 +1,14 @@
 package dominio;
 
+import java.util.List;
+
 public class Categoria {
 	
 	// Internal Variables
 	private Integer codCategoria;
 	private String nome;
+	
+	// Relationship Variables
+	private List<Livro>livros;
 
 }
