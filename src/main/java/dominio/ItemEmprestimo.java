2068cb587785c01f51e668a30bc3b45108a31e9a
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 18:47:55 -0300

    Add database relationship in ItemEmprestimo class

--- src/main/java/dominio/ItemEmprestimo.java
+++ src/main/java/dominio/ItemEmprestimo.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,7 +22,11 @@ public class ItemEmprestimo implements Serializable {
 	private Integer quantidade;
 	
 	// Relationship Variables
+	@ManyToOne
+	@JoinColumn(name="livro")
 	private Livro livro;
+	@ManyToOne
+	@JoinColumn(name="emprestimo")
 	private Emprestimo emprestimo;
 	
 	public ItemEmprestimo(){}
