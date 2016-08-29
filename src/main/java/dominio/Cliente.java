9a933036f71ee55cb51b23188de3060e749f4fa5
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:36:53 -0300

    Create Constructors in Cliente class
    Create Constructors (empty and filled) in Cliente class

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -1,6 +1,7 @@
 package dominio;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,5 +15,25 @@ public class Cliente {
 	
 	// Relationship Variables
 	private List<Emprestimo>emprestimos;
+	
+	Cliente()
+	{
+		this.emprestimos = new ArrayList<>();
+	}
+
+	public Cliente(Integer codCliente, String nome, String cpf, String fone, String email, Date nascimento,
+			BigDecimal rendaMensal) {
+		super();
+		this.codCliente = codCliente;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.fone = fone;
+		this.email = email;
+		this.nascimento = nascimento;
+		this.rendaMensal = rendaMensal;
+		this.emprestimos = new ArrayList<>();
+	}
+	
+	
 
 }
