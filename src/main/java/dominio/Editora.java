8ea015aed7461851c33d16f3ab2edff4151555bf
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 18:41:58 -0300

    Add database relationship in Editora class

--- src/main/java/dominio/Editora.java
+++ src/main/java/dominio/Editora.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +23,7 @@ public class Editora implements Serializable {
 	private String nome;
 	
 	// Relationship Variables
+	@OneToMany(mappedBy="editora")
 	private List<Livro>livros;
 	
 	public Editora()
