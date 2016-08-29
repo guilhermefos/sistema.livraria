b1cc5a2bad8241320c559a00b2c36caba7c77aa4
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 13:46:54 -0300

    Add Getting and Setters in Cliente class
    Adding getting and setters methods in Cliente class

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -33,6 +33,120 @@ public class Cliente {
 		this.rendaMensal = rendaMensal;
 		this.emprestimos = new ArrayList<>();
 	}
+
+	/**
+	 * @return the codCliente
+	 */
+	public Integer getCodCliente() {
+		return codCliente;
+	}
+
+	/**
+	 * @param codCliente the codCliente to set
+	 */
+	public void setCodCliente(Integer codCliente) {
+		this.codCliente = codCliente;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	/**
+	 * @return the fone
+	 */
+	public String getFone() {
+		return fone;
+	}
+
+	/**
+	 * @param fone the fone to set
+	 */
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the nascimento
+	 */
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	/**
+	 * @param nascimento the nascimento to set
+	 */
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	/**
+	 * @return the rendaMensal
+	 */
+	public BigDecimal getRendaMensal() {
+		return rendaMensal;
+	}
+
+	/**
+	 * @param rendaMensal the rendaMensal to set
+	 */
+	public void setRendaMensal(BigDecimal rendaMensal) {
+		this.rendaMensal = rendaMensal;
+	}
+
+	/**
+	 * @return the emprestimos
+	 */
+	public List<Emprestimo> getEmprestimos() {
+		return emprestimos;
+	}
+
+	/**
+	 * @param emprestimos the emprestimos to set
+	 */
+	public void setEmprestimos(List<Emprestimo> emprestimos) {
+		this.emprestimos = emprestimos;
+	}
+	
+	
 	
 	
 
