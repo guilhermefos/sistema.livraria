ee039b139df7c7eb51a7cfe75a99a36ce2da40c4
guilhermefos <guilhermefos.developer@gmail.com>
2016-08-29 18:29:40 -0300

    Add @Entity, @Table with name and creating primary_key in Livro class

--- src/main/java/dominio/Livro.java
+++ src/main/java/dominio/Livro.java
@@ -5,10 +5,20 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_livros")
 public class Livro implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	// Internal Variables
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer codLivro;
 	private String titulo, isbn;
 	private Integer paginas;
