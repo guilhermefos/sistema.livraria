a3dd8673af37ed3781d6dafdae4a6f59ac0d9f90
guilhermefos <guilhermefos.developer@gmail.com>
2016-09-21 20:39:33 -0300

    Create and implement begin function

--- src/main/java/dao/Transaction.java
+++ src/main/java/dao/Transaction.java
@@ -1,5 +1,18 @@
 package dao;
 
+import dao.impl.EM;
+
 public class Transaction {
+	
+	/**
+	 * Begin marks the starting point of an explicit, local transaction
+	 * 
+	 * @return void
+	 * 
+	 */
+	public static void begin()
+	{
+		EM.getLocalEm().getTransaction().begin();
+	}
 
 }
